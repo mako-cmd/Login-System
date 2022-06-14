37e5b6aa4ca577508f2c6856c2e5f70e60dc6858
mako-cmd <makomboreroman@gmail.com>
2022-06-14 23:21:02 +1000

    Project complete, in future will implement hashing algos

--- Login_System/Login_System.cpp
+++ Login_System/Login_System.cpp
@@ -1,20 +1,122 @@
-// Login_System.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+#if __cplusplus > 201402L // If the version of C++ is > than 14
+    #include <experimental/filesystem>
+    // It was still in the experimental:: namespace
+    namespace fs = std::experimental::filesystem;
+#else
+    #include <filesystem>
+    namespace fs = std::filesystem;
+#endif
 
-int main()
+using namespace std;
+
+int main(int argc, char * argv[])
 {
-    std::cout << "Hello World!\n";
+    const unsigned int password_len = 15;
+    struct stat info;
+    char input;         //buffer for user inputs
+    char data[password_len];      //buffer for reading password
+    string name_buffer; //buffer for username input
+    string pass_buffer; //buffer for password input
+    string buffer;      //working buffer
+    cout << "Login System 1.0" << endl;
+    cout << "Login? (L)" << endl;
+    cout << "Register? (R)" << endl;
+    cin >> input;
+    ofstream create;    //obj to create and write to credential files
+    ifstream read;      //obj to read credential files
+    //****************************************************************/
+    //**********************Registration branch***********************/
+    //****************************************************************/
+    if (input == 'R' || input == 'r')
+    {
+        name_buffer.clear();    //empty buffer
+        pass_buffer.clear();    //empty buffer
+        system("cls");
+        cout << "Choose a username: ";
+        cin >> name_buffer;
+        system("cls");
+        cout << "Choose a password: ";
+        cin >> pass_buffer;
+        //Save credentials in file named name_buffer
+        if (create.is_open())   //If stream is open close it
+        {
+            create.flush();
+            create.close();
+        }
+        //Check if Credentials folder exists
+        //If folder is not there make another one
+        if (stat(".\\Credentials\\", &info) != 0)
+        {
+            fs::create_directory("Credentials");
+        }
+        //create path to credential file to be created
+        buffer = ".\\Credentials\\";
+        buffer.append(name_buffer);
+        buffer.append(".cred");
+        create.open(buffer.c_str(),ofstream::out);
+        //Write password 
+        create.write(pass_buffer.c_str(), pass_buffer.size());
+        //Close stream
+        create.flush();
+        create.close();
+        
+        //Success
+        system("cls");
+        printf("User %s Registered!",name_buffer.c_str());
+    }
+    //****************************************************************/
+    //*************************Login branch***************************/
+    //****************************************************************/
+    else if (input == 'L' || input == 'l')
+    {
+        name_buffer.clear();    //empty buffer
+        pass_buffer.clear();    //empty buffer
+        memset(data, 0, password_len);
+        system("cls");
+        cout << "Username: ";
+        cin >> name_buffer;
+        system("cls");
+        cout << "Password: ";
+        cin >> pass_buffer;
+        //check if name file exists
+        buffer = ".\\Credentials\\";
+        buffer.append(name_buffer);
+        buffer.append(".cred");
+        //if username doent exist
+        if (!(fs::exists(buffer.c_str())))
+        {
+            system("cls");
+            cout << "Login Failed: Username does not exist" << endl;
+        }
+        else
+        {
+            //Validate password
+            if (read.is_open())   //If stream is open close it
+            {
+                read.close();
+            }
+            read.open(buffer.c_str(), ifstream::in);    //open credential file
+            //Get password
+            read.read(data, password_len);
+            //close stream
+            read.close();
+            //Validate password
+            //If password is not valid
+            if (strcmp(data, pass_buffer.c_str()))
+            {
+                system("cls");
+                cout << "Login Failed: Incorrect Password" << endl;
+            }
+            else
+            {
+                system("cls");
+                cout << "Logged in successfully" << endl;
+            }
+        }
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
